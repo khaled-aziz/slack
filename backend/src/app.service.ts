d80be743efb1d26f489479d009108a46ca59ed94
Kh.Aziz <khaled.aziz.gomaa@gmail.com>
2025-11-30 19:30:17 +0200

    add confilct

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 11+11+'Hello git v2 from feature-hello branch !';
   }
   postHello(): string {
     return 'Hello git v2 from feature-hello branch !';
