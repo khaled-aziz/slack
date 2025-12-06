49a90b053e10c9f856917c6b16bcba6c20b39ec0
Kh.Aziz <khaled.aziz.gomaa@gmail.com>
2025-12-06 18:27:34 +0200

    update greeting messages to reflect feature-tst-pr branch

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,15 +3,15 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 1+'Hello git v1.2.0 from feature-hello branch !';
+    return 1+'Hello git v1.2.0 from feature-tst-pr branch !';
   }
   postHello(): string {
-    return 'Hello from feature-hello branch !';
+    return 'Hello from feature-tst-pr branch !';
   }
   // putHello(): string {
-  //   return 'Hello from feature-hello branch !';
+  //   return 'Hello from feature-tst-pr branch !';
   // }
   deleteHello(): string {
-    return 'Hello git v1.2.0 from feature-hello branch !';
+    return 'Hello git v1.2.0 from feature-tst-pr branch !';
   }
 }
